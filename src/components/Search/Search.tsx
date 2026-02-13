5282d3ada084af4e4ba618164dc12d69145ffd92
Elijah Prince <elijahprince73@gmail.com>
2026-02-12 23:11:14 -0800

    Senior Level App

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,6 +1,10 @@
-const Search = () => {
+interface SearchProps {
+  onSearchCallback: (value: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Search = ({ onSearchCallback }: SearchProps) => {
   return (
-    <input className="my-4 border " type="text" placeholder="Search for user"/>
+    <input className="my-4 border " type="text" placeholder="Search for user" onChange={(e) => onSearchCallback(e.target.value)}/>
   )
 }
 export default Search
